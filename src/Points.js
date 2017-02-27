6f252c315c3110bc50b9db2f2c3b29f8a7e516d1
Gibral Raphael <gibral.raphael@gmail.com>
2017-02-27 11:38:17 -0500

    added ability to see questions

--- src/Points.js
+++ src/Points.js
@@ -5,15 +5,8 @@ class Points extends Component{
 	render() {
 		return (
 			
-			<div className="points-container" ontouchstart="this.classList.toggle('hover');">
-				<div className="points">
-					<div className="frontcard">
-						<h2>Hello</h2>
-					</div>
-					<div className="backcard">
-						<h2>Bye</h2>
-					</div>
-				</div>
+			<div className="points-container" >
+				<h2>Points</h2>
 			</div>
 			
 		);
